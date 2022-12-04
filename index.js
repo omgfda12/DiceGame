f08368ffeccf6a097f2eb9ae29834055d47782db
omgfda12 <114806616+omgfda12@users.noreply.github.com>
2022-12-04 22:46:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var two= prompt("Enter Player 2 Name");
 document.querySelector("h2.Pone").innerHTML=one;
 document.querySelector("h2.Ptwo").innerHTML=two;
 var randomNo1=Math.floor(Math.random()*6)+1;
-document.querySelectorAll("img")[0].setAttribute("src","images/dice"+randomNo1+".png");
+document.querySelectorAll("img")[0].setAttribute("src","dice"+randomNo1+".png");
 
 var randomNo2=Math.floor(Math.random()*6)+1;
-document.querySelectorAll("img")[1].setAttribute("src","images/dice"+randomNo2+".png");
+document.querySelectorAll("img")[1].setAttribute("src","dice"+randomNo2+".png");
 
 if(randomNo1==randomNo2)
 {
